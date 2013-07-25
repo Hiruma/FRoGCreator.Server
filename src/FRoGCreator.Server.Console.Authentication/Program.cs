cb65eb53cdf8b7548d06e5f0334023ffd8de7b09
Clemzy <neo.area.prog@gmail.com>
2013-07-25 14:32:24 +0200

    Brouillon : Lecture & Accept Async (Sans Test, Sans crypt, ...)
    Réaliser la classe externe Configs, Functions ~
    --> Étude de la méthode de sécurité à employer,
    Forme de la BDD. 
    --> Communication interprocess ou sockets locaux entre consoles?
    
    [...]

--- src/FRoGCreator.Server.Console.Authentication/Program.cs
+++ src/FRoGCreator.Server.Console.Authentication/Program.cs
@@ -2,14 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+/*using System.Threading;*/
 using System.Threading.Tasks;
+/*using System.Net;
+using System.Net.Sockets;*/
 
 namespace FRoGCreator.Server.Console.Authentication
 {
     class Program
     {
+        // private static ManualResetEvent _Valider = new ManualResetEvent(false);
         static void Main(string[] args)
         {
+            /*try
+            {
+                Socket Listener = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream, 
+                    ProtocolType.Tcp);
+                Listener.Bind(new IPEndPoint(Configs.IPALLOWED, Configs.PORT));
+                Listener.Listen(Configs.WAITLENGHT);
+                
+                while(true)
+                {
+                    _Valider.Reset();
+                    Listener.BeginAccept(new AsyncCallback(AsyncAccept), Listener);
+                    _Valider.WaitOne();
+                }
+            }
+            catch(Exception ex)
+            {
+                return; // Display Error
+            }*/
         }
+        
+        /*public static void AsyncAccept(IAsyncResult Result_)
+        {
+            try
+            {
+                _Valider.Set();
+                Socket Client = ((Socket)Result_.AsyncState).EndAccept(Result_);
+                //Add Client in Dict 
+                //       ...         
+                //NetworkPath Client >> Client.RemoteEndPoint();     
+            }
+            catch (Exception ex)
+            {
+                return; // Display Error
+            }
+        }*/
     }
 }
+
+/* (Classe externe Configs.cs)
+    
+    using System.Net;
+    
+    static string TITLE = "Server Caption";
+    static int PORT = 5555,
+         WAITLENGHT = 99;
+    static IPAddress IPALLOWED = IPAddress.Any;
+    
+    Public static void Initialize()
+    {
+        Console.Title = TITLE;
+        ...
+    }
+    
+*/
