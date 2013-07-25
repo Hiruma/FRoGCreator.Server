89c83580611785aa56b3d57a133158da1453140d
Clemzy <neo.area.prog@gmail.com>
2013-07-25 21:04:48 +0200

    Update Program.cs

--- tests/Program.cs
+++ tests/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using FRoGCreator.Server.Console.Authentication.Classes.Functions;
 
 namespace FRoGCreator.Server.Console.Authentication
 {
