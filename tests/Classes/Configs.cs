08617d56a99ad721faebabec7807506c5bfbceb2
Clemzy <neo.area.prog@gmail.com>
2013-08-08 12:09:18 +0200

    BROUILLON CONFIGS.CS @\CLASSES

---
+++ tests/Classes/Configs.cs
@@ -0,0 +1,28 @@
+/* BROUILLON CONFIGS.CS @\CLASSES*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRoGCreator.Server.Core.Classes
+{
+    public static class Configs
+    {
+        public static string 
+            DATABASE
+            = "Database.db",
+            PASSWORD
+            = "";
+
+        public static string
+            TITLE
+            = "Serveur d'authentification",
+            HASHPASS
+            = "FROG";
+
+        public static uint
+            PORT = 5555;
+    }
+}
